851cf2a4bd88066960397a7b4dbf6007871a70f8
wangbing <403947308@qq.com>
2023-06-18 10:47:06 +0800

    提交bean定义解析部分的代码

--- core/src/main/java/com/wb/springframework/core/type/classreading/CachingMetadataReaderFactory.java
+++ core/src/main/java/com/wb/springframework/core/type/classreading/CachingMetadataReaderFactory.java
@@ -1,9 +1,15 @@
 package com.wb.springframework.core.type.classreading;
 
+import java.io.IOException;
+
 /**
  * @author WangBing
  * @date 2023/6/16 23:21
  */
 public class CachingMetadataReaderFactory implements MetadataReaderFactory {
 
+    @Override
+    public MetadataReader getMetadataReader(String className) throws IOException {
+        return null;
+    }
 }
